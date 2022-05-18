d0ad35244866c47748ba256ddc61771106455adb
arman <arman@HONOR>
2022-05-17 19:10:18 -0500

    Master

---
+++ calculadora/src/main/java/com/example/calculadora/CalculadoraController.java
@@ -0,0 +1,27 @@
+package com.example.calculadora;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class CalculadoraController {
+
+	@GetMapping("/")
+	public String index(Model modelo)
+	{
+		modelo.addAttribute("calcu", new Calculadora());
+		return "/index";
+	}
+	
+	@PostMapping("/resultado")
+	public String resultado(@ModelAttribute Calculadora calcu, Model modelo)
+	{
+		calcu.Calcular();
+		modelo.addAttribute("calcu", calcu);
+		return "/index";
+		}
+}
