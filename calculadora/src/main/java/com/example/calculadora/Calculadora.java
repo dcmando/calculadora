3d0f9c904145a1afbcef569fa81e573b7d5dd9aa
Armando Joel Rodriguez Sanchez <77363588+dcmando@users.noreply.github.com>
2022-05-30 19:24:09 -0500

    Update Calculadora.java

--- calculadora/src/main/java/com/example/calculadora/Calculadora.java
+++ calculadora/src/main/java/com/example/calculadora/Calculadora.java
@@ -1,24 +1,24 @@
 package com.example.calculadora;
 
 public class Calculadora {
-	private int num1;
-	private int num2;
+	private double num1;
+	private double num2;
 	private char operador;
-	private float resultado;
+	private double resultado;
 	
-	public int getNum1() {
+	public double getNum1() {
 		return num1;
 	}
 	
-	public void setNum1(int num1) {
+	public void setNum1(double num1) {
 		this.num1 = num1;
 	}
 	
-	public int getNum2() {
+	public double getNum2() {
 		return num2;
 	}
 	
-	public void setNum2(int num2) {
+	public void setNum2(double num2) {
 		this.num2 = num2;
 	}
 	
@@ -34,7 +34,7 @@ public class Calculadora {
 		return resultado;
 	}
 	
-	public void setResultado(float resultado) {
+	public void setResultado(double resultado) {
 		this.resultado = resultado;
 	}
 
